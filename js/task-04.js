bb095b2aaf340c3a0adaaa93cf0a90b147314a44
MykhailoVasyliuk <110117416+MykhailoVasyliuk@users.noreply.github.com>
2022-12-08 23:40:50 +0200

    completed task-04

---
+++ js/task-04.js
@@ -0,0 +1,20 @@
+const decrementEl = document.querySelector(`button[data-action="decrement"]`);
+const incrementEl = document.querySelector(`button[data-action="increment"]`);
+const valueEl = document.querySelector(`#value`);
+
+let counterValue = 0;
+
+decrementEl.addEventListener(`click`, minusBtnClick);
+incrementEl.addEventListener(`click`, plusBtnClick);
+
+
+function minusBtnClick () {
+    counterValue -= 1;
+    valueEl.textContent = counterValue;
+}
+
+function plusBtnClick () {
+    counterValue += 1;
+    valueEl.textContent = counterValue;
+}
+
