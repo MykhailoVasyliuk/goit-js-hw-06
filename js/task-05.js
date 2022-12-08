11d98aa8201fa34331257fcfb24d29c31c38811a
MykhailoVasyliuk <110117416+MykhailoVasyliuk@users.noreply.github.com>
2022-12-09 00:26:02 +0200

    edit task-05

--- js/task-05.js
+++ js/task-05.js
@@ -2,8 +2,10 @@ const textInputEl = document.querySelector("#name-input");
 const textOutputEl = document.querySelector("#name-output")
 
 textInputEl.addEventListener(`input`, (event) => {
+    textOutputEl.textContent = event.currentTarget.value;
+    
     if (event.currentTarget.value === "") {
         textOutputEl.textContent = "Anonymous";
     }
-    textOutputEl.textContent = event.currentTarget.value;
+    
 })
