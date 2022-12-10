8e1cc108c387cfec498b3261ffcb85941e58f298
MykhailoVasyliuk <110117416+MykhailoVasyliuk@users.noreply.github.com>
2022-12-10 03:40:59 +0200

    completed task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,27 @@
 
+const formEl = document.querySelector(".login-form");
+
+formEl.addEventListener("submit", onFormSubmit);
+
+function onFormSubmit(event) {
+    event.preventDefault();
+
+    const formElements = event.currentTarget.elements;
+
+    const email = formElements.email.value;
+    const password = formElements.password.value;
+
+    const formData = {
+        email,
+        password
+    }
+
+    if (formElements.email.value === "" || formElements.password.value === "") {
+        alert("Всі поля повинні бути заповнені!");
+    } else {
+        console.log(formData);
+    }
+
+        formEl.reset();
+}
+
